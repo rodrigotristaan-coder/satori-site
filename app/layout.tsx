f64cc8157078db08a84f4386bb16610a716de08d
Rodrigo Tristán <r.tristaan@outlook.com>
2026-05-05 23:27:55 -0600

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" className="scroll-smooth">
-      <head>
-        <link rel="icon" href="/icon.png" type="image/png" />
-        <link rel="shortcut icon" href="/icon.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-      </head>
       <body>{children}</body>
     </html>
   );
